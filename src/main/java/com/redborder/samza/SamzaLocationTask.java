8147477c1f2e8313546c40e3726c7425e114610f
Andrés Gomez <agomez@redborder.com>
2016-03-09 13:21:56 +0100

    Added dimToEnrich logic.

--- src/main/java/com/redborder/samza/SamzaLocationTask.java
+++ src/main/java/com/redborder/samza/SamzaLocationTask.java
@@ -8,6 +8,7 @@ import org.apache.samza.system.OutgoingMessageEnvelope;
 import org.apache.samza.system.SystemStream;
 import org.apache.samza.task.*;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -19,18 +20,20 @@ public class SamzaLocationTask implements StreamTask, InitableTask {
     final SystemStream systemStream = new SystemStream("kafka", "rb_loc_post");
     KeyValueStore<String, Map<String, Object>> store;
     Long consolidatedTime;
+    List<String> dimToEnrich;
 
     @Override
     public void init(Config config, TaskContext taskContext) throws Exception {
         this.store = (KeyValueStore<String, Map<String, Object>>) taskContext.getStore("location");
         this.consolidatedTime = config.getLong("redborder.location.consolidatedTime", 3 * MINUTE);
+        this.dimToEnrich = config.getList("redborder.location.dimToEnrich", Collections.<String>emptyList());
     }
 
     @Override
     public void process(IncomingMessageEnvelope envelope, MessageCollector collector, TaskCoordinator taskCoordinator) throws Exception {
         Map<String, Object> message = (Map<String, Object>) envelope.getMessage();
         String client = (String) message.get(CLIENT);
-        String namespace =  message.get(NAMESPACE) == null ? "" : (String) message.get(NAMESPACE);
+        String namespace = message.get(NAMESPACE) == null ? "" : (String) message.get(NAMESPACE);
 
         String id = client + namespace;
 
@@ -49,6 +52,14 @@ public class SamzaLocationTask implements StreamTask, InitableTask {
 
 
             for (Map<String, Object> event : events) {
+
+                for (String dim : dimToEnrich) {
+                    Object dimValue = message.get(dim);
+                    if (dimValue != null) {
+                        event.put(dim, dimValue);
+                    }
+                }
+
                 collector.send(new OutgoingMessageEnvelope(systemStream, event.get(CLIENT), event));
             }
         }
